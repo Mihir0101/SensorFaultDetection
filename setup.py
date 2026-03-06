17069a6982d510fdf50e98aee9982bc11b56949e
Mihir0101 <parmarmihir31616@gmail.com>
2026-03-06 17:57:40 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+hypn_e_dot = '-e .'
+
+def get_requirements(file_path:str) -> List[str] :
+
+    requirements = []
+
+    with open(file_path) as file_obj :
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+        if hypn_e_dot in requirements :
+            requirements.remove(hypn_e_dot)
+
+        return requirements
+
+setup(
+    name = 'src',
+    version = '0.0.1',
+    author = 'Mihir Parmar',
+    author_email= 'connectwithmihir1@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
